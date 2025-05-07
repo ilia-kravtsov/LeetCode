3b93db89fcf031841c5cd67d1f6e8a26d9bac63c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2025-05-07 11:48:34 +0300

    Card_Track\Merge Sorted Array.js add additional method

--- Card_Track/Merge Sorted Array.js
+++ Card_Track/Merge Sorted Array.js
@@ -38,7 +38,7 @@ Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
 // Runtime: 0 ms Memory Usage: 56 MB
 
-var merge = function merge(nums1, m, nums2, n) {
+function merge_1(nums1, m, nums2, n) {
   let i = m - 1;
   let j = n - 1;
   let k = m + n - 1;
@@ -51,3 +51,20 @@ var merge = function merge(nums1, m, nums2, n) {
     }
   }
 };
+
+function merge_2(nums1, m, nums2, n) {
+  let i = m - 1; 
+  let j = n - 1; 
+  let k = m + n - 1; 
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+}
